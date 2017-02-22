9d94fc79ad4cbf6c7b7ebcaa5cfc98bfc3556dc8
marcy <marcy.montross@gmail.com>
2017-02-21 18:05:18 -0600

    adding tests

---
+++ test/blog_test.js
@@ -0,0 +1,30 @@
+const chai = rquire('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+const {app, runServer, closeServer} = require('../server');
+
+
+
+describe('Blog Posts', function() {
+    before(function() {
+        return runServer();
+    });
+    after(function() {
+        return closeServer();
+    });
+    it('Display blog posts on GET', function(){
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function(res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                const expectedKeys = ['id', 'title', 'content', 'author'];
+                res.body.forEach(function(item) {
+                    item.should.be.a('object');
+                    item.should.include.keys(expectedKeys);
+                });
+            });
+    });
+});
