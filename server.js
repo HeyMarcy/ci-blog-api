80828b28d05d6a99d3d40206d53e4299c62afd6a
marcy <marcy.montross@gmail.com>
2017-02-21 18:12:31 -0600

    debug tests

--- server.js
+++ server.js
@@ -7,9 +7,46 @@ const blogPostRouter = require('./blogPostRouter');
 
 // log the http layer
 app.use(morgan('common'));
-app.use('/blogposts', blogPostRouter);
 
 app.use('/blogposts', blogPostRouter);
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+
+
+
+
+ let server;
+
+  function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve();
+    })
+    .on('error', err => {
+      reject(err);
+    });
+  });
+}
+
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    // server.close(err => {
+    //   if (err) {
+    //     reject(err);
+    //     // so we don't also call `resolve()`
+    //     return;
+    //   }
+      resolve();
+    });
+  };
+
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {runServer, app, closeServer};
